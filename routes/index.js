f1d6a8994770857eca12b807dd1d8ff689ad9e09
NoMoreViolence <ljh86029926@gmail.com>
2017-09-24 15:28:52 +0900

    Donation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// 홈페이지
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { donation: false });
+});
+
+// 도네이션
+router.get('/donation', function(req, res, next) {
+    res.render('index', { donation: true });
 });
 
 module.exports = router;
