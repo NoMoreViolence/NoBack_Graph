e89c83e834ef2cbcf78efe31c92e8473ea4bfad6
NoMoreViolence <ljh86029926@gmail.com>
2017-09-26 07:54:19 +0900

    ESLint ES6

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": "airbnb-base",
+
+  "rules": {
+    "no-console": "off"
+  }
+
+};
+/* 
+{
+  "rules" = {
+    "no-console": 0
+  }
+}
+*/
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,64 @@
-var express = require('express');
-var router = express.Router();
+// 모듈
+const express = require('express');
+
+const router = express.Router();
 
 // 홈페이지
-router.get('/', function(req, res, next) {
-    res.render('index', { donation: false });
+router.get('/', (req, res) => {
+  // game, rank, donation, setting
+  // ejs에 각각 로딩할 페이지 여부
+  res.render('index', {
+    game: false,
+    rank: false,
+    donation: false,
+    setting: false,
+  });
+});
+// favicon.ico 오류 처리
+router.get('/favicon.ico', (req, res) => {
+  console.log('favicon error cleaned');
+  console.log('abcd');
+  res.status(204);
+});
+
+// 게임 시작
+router.get('/start', (req, res) => {
+  res.render('index', {
+    game: true,
+    rank: false,
+    donation: false,
+    setting: false,
+  });
+});
+
+// 랭킹
+router.get('/rank', (req, res) => {
+  res.render('index', {
+    game: false,
+    rank: true,
+    donation: false,
+    setting: false,
+  });
 });
 
 // 도네이션
-router.get('/donation', function(req, res, next) {
-    res.render('index', { donation: true });
+router.get('/donation', (req, res) => {
+  res.render('index', {
+    game: false,
+    rank: false,
+    donation: true,
+    setting: false,
+  });
+});
+
+// 세팅
+router.get('/setting', (req, res) => {
+  res.render('index', {
+    game: false,
+    rank: false,
+    donation: false,
+    setting: true,
+  });
 });
 
 module.exports = router;
