35d8c843c73526bb12c372da24e93877d51eaf58
NoMoreViolence <ljh86029926@gmail.com>
2017-10-02 03:04:46 +0900

    socket connections

--- public/javascripts/gamescript.js
+++ public/javascripts/gamescript.js
@@ -3,7 +3,7 @@ window.onload = () => {
   const socket = io();
 
 
-  // jquery
+  // 게임 기록 버튼 and 게임 채팅 버튼
   $('#btn1').click(() => {
     $('.record').css('display', 'block');
     $('.chat').css('display', 'none');
@@ -11,5 +11,26 @@ window.onload = () => {
   $('#btn2').click(() => {
     $('.record').css('display', 'none');
     $('.chat').css('display', 'block');
+  });
+
+  // 채팅 메시지 보낼 때 Enter
+  $('.tongue').keydown((key) => {
+    if (key.keyCode === 13) {
+      // input 메시지
+      const msg = $('.tongue').val();
+      // socket 전송
+      socket.emit('SendMessage', { message: msg });
+    }
+  });
+  // 버튼 클릭
+  $('.sound').click(() => {
+    // input 메시지
+    const msg = $('.tongue').val();
+    // socket 전송
+    socket.emit('SendMessage', { message: msg });
+  });
+  // socket 응답
+  socket.on('ResMessage', (data) => {
+    $('.ear').append(data);
   });
 };
